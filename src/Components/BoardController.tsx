7856fa7bfc0b76f1be1635a64417803490ffdfc3
Eluwing <abek0403@gmail.com>
2022-12-20 20:45:38 +0900

    [Feature] Add Form that ADD/DELETE

--- src/Components/BoardController.tsx
+++ src/Components/BoardController.tsx
@@ -1,37 +1,87 @@
-import React from "react";
+import React, { HtmlHTMLAttributes, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
+import { isPropertySignature } from "typescript";
 import { toDoState } from "../atoms";
+
+const ADD_FORM_NAME = "Add";
+const DELETE_FORM_NAME = "Delete"
+
+interface IBoardControllerForm {
+    targetBoardName: string;
+};
 
 const Form = styled.form`
 `;
 
-const handleBoardDelete = () =>{
-    console.log("Board Delete!");
-
-}
-
-const BoardController = () =>{
+const BoardController = () => {
     const setToDos = useSetRecoilState(toDoState);
-    const { register, setValue, handleSubmit } = useForm();
+    const { register, setValue, handleSubmit } = useForm<IBoardControllerForm>();
+    const [isDeleteVisible, setIsDeleteVisible] = useState(false);
+    const [formStatus, setFormStatus] = useState<string>(ADD_FORM_NAME);
 
-    const onBoardAdd = ({addBoardName}:any) =>{
-        setToDos((allOldBoards)=>{
-            return{
-                ...allOldBoards,
-                [addBoardName]:[],
-            }
-        })
+    const onBoardController = ({ targetBoardName }: IBoardControllerForm) => {
+        console.log({formStatus});
+        if(formStatus === ADD_FORM_NAME){
+            //Add taget Key in toDo Array
+            setToDos((allOldBoards) => {
+                return {
+                    ...allOldBoards,
+                    [targetBoardName]: [],
+                }
+            });
+        }
+        else if(formStatus === DELETE_FORM_NAME){
+            console.log("Delete button");
+            //Delete taget Key in toDo Array
+            setToDos((allOldBoards) => {
+                return {
+                    ...allOldBoards,
+                    [targetBoardName]: [],
+                }
+            });
+        }
+        return;
+
     };
 
-    return(
+    const onButtonChange = (event: React.MouseEvent<HTMLElement>) => {
+        setIsDeleteVisible((oldDeleteVisible) => {
+            return !oldDeleteVisible;
+        })
+        setFormStatus((event.target as HTMLInputElement).name);
+    };
+
+    const DeleteForm = () => {
+        return (
+            <>
+                <button onClick={onButtonChange} type="button" name={DELETE_FORM_NAME}>Change Add Form</button>
+                <div>
+                    <button type="submit">Board Delete</button>
+                </div>
+            </>
+        );
+    };
+
+    const AddForm = () => {
+        return (
+            <>
+                <button onClick={onButtonChange} type="button" name={ADD_FORM_NAME}>Change Delete Form</button>
+                <div>
+                    <button type="submit">Board Add</button>
+                </div>
+            </>
+        );
+    };
+
+    console.log({formStatus});
+    return (
         <>
-            <Form onSubmit={handleSubmit(onBoardAdd)} >
-                <input {...register("addBoardName",{required: true})} type="text" placeholder="Input Add board Name"></input>
-                <button type="submit">Board Add</button>
+            <Form onSubmit={handleSubmit(onBoardController)}>
+                <input {...register("targetBoardName", { required: true })} type="text" placeholder="Input Add board Name"></input>
+                {isDeleteVisible ? <DeleteForm />: <AddForm />}
             </Form>
-            <button onClick={handleBoardDelete}>Board Delete</button>
         </>
     );
 };
